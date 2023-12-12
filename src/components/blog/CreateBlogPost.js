a5abed6118820f541404261d406fd517e287435e
VanessaA13 <vanessa.akhras@outlook.com>
2023-12-11 21:25:33 -0500

    navigate

--- src/components/blog/CreateBlogPost.js
+++ src/components/blog/CreateBlogPost.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
